9a5ec34748d05905448afa38392e4b9d9a9b9325
Kamal <kamaljit.singh@auxiliobits.com>
2025-05-27 16:27:05 +0530

    Update googlesheety.py

--- googlesheety.py
+++ googlesheety.py
@@ -9,7 +9,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import logger 
+from logging import Logger
 
 
 # Load environment variables
